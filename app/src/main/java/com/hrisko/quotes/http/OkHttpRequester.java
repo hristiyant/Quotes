a0b3329a38989f52c7a4c39f2adfe19779e29a5a
vladimirVenkov <vladimir.ognyanov.venkov@gmail.com>
2018-09-04 16:09:20 +0300

    OkHttp impl get and post

--- app/src/main/java/com/hrisko/quotes/http/OkHttpRequester.java
+++ app/src/main/java/com/hrisko/quotes/http/OkHttpRequester.java
@@ -4,6 +4,12 @@ import com.hrisko.quotes.http.base.HttpRequester;
 
 import java.io.IOException;
 
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class OkHttpRequester implements HttpRequester {
 
     public OkHttpRequester(){
@@ -12,16 +18,40 @@ public class OkHttpRequester implements HttpRequester {
 
     @Override
     public String get(String url) throws IOException {
+        Request request = new Request.Builder()
+                .get()
+                .url(url)
+                .build();
+        OkHttpClient client = new OkHttpClient();
+
+        Response response = client.newCall(request)
+                .execute();
+        String body = response.body().string();
+        return body;
+    }
+
+    @Override
+    public String put(String url, String bodyString) throws IOException {
         return null;
     }
 
     @Override
-    public String put(String url, String body) throws IOException {
-        return null;
-    }
+    public String post(String url, String bodyString) throws IOException {
+        RequestBody body = RequestBody.create(
+                MediaType.parse("application/json"),
+                bodyString
+        );
 
-    @Override
-    public String post(String url, String body) throws IOException {
-        return null;
+        Request request = new Request.Builder()
+                .post(body)
+                .url(url)
+                .build();
+        OkHttpClient client = new OkHttpClient();
+
+        Response response = client.newCall(request)
+                .execute();
+        String responseBody = response.body().string();
+
+        return responseBody;
     }
 }
